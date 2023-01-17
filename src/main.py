fb4eb8fd1d6aad12c6ebf555221a6e1d839e5c51
Stanisław Kumor <stanislav.kumor@gmail.com>
2023-01-17 17:08:52 +0100

    test

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 from src.allegro_api_reader.api_authoriser import get_access_token
 from src.allegro_api_reader.api_reader import get_main_categories
 
+ACCESS_TOKEN = get_access_token()
 
-def main():
-    access_token = get_access_token()
-    print("access token = " + access_token)
-    main_categories = get_main_categories(access_token)
+
+def test_main():
+    # access_token = get_access_token()
+    print("access token = " + ACCESS_TOKEN)
+    main_categories = get_main_categories(ACCESS_TOKEN)
     print(main_categories)
 
 
+def main():
+    pass
 
 if __name__ == "__main__":
     main()
+    test_main()

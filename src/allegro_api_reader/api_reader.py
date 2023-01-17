ef10bfd50b91626c11c75d92ffc0de027ed02a6d
Stanisław Kumor <stanislav.kumor@gmail.com>
2023-01-17 17:58:21 +0100

    get_all_categories updated

--- src/allegro_api_reader/api_reader.py
+++ src/allegro_api_reader/api_reader.py
@@ -6,13 +6,11 @@ def map_to_dataframe(dictionary):
     return pd.DataFrame.from_dict(dictionary)
 
 
-def get_main_categories(token):
+def get_all_categories(token):
     try:
         url = "https://api.allegro.pl/sale/categories"
         headers = {'Authorization': 'Bearer ' + token, 'Accept': "application/vnd.allegro.public.v1+json"}
-        main_categories_result = (requests.get(url, headers=headers, verify=False)).json()
-        main_categories_result = map_to_dataframe(main_categories_result)
-
-        return main_categories_result
+        categories_dict = (requests.get(url, headers=headers, verify=False)).json()
+        return map_to_dataframe(categories_dict)
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
